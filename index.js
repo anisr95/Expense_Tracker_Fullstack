22e609cc6fe813ba24f6d42bbf5d541afd41f96c
Sina Rafieefar <anisr95@yahoo.com>
2022-06-23 11:57:43 -0700

    minor changes

--- index.js
+++ index.js
@@ -58,13 +58,14 @@ app.get('/login', (req, res, next) => {
 
 // __dirname = path.resolve(path.dirname(''));
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.resolve(__dirname, "/client/build")));
+    app.use(express.static(path.join(__dirname, "client", "build")))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     })
 }
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+    console.log(__dirname)
 });
