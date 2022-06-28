88e601280e83eed9122918ccf60f46f674b693a3
Sina Rafieefar <anisr95@yahoo.com>
2022-06-27 21:27:33 -0700

    Login and Signup components added

---
+++ client/src/components/Auth/Signup.jsx
@@ -0,0 +1,64 @@
+import { Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Link, Navigate, NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Signup = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [haveAccount, setHaveAccount] = useState(true);
+
+  const navigate = useNavigate();
+
+  const submitHandler = async () => {
+    const newUser = {
+      username,
+      password,
+    };
+    await axios.post("/auth/register", newUser);
+
+    setUsername("");
+    setPassword("");
+    navigate("/expenses");
+  };
+
+  return (
+    <>
+      <Typography
+        variant="h3"
+        color="primary.light"
+        sx={{ letterSpacing: "4px" }}
+      >
+        Sign Up
+      </Typography>
+      <TextField
+        variant="outlined"
+        label="Username"
+        fullWidth
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+      />
+      <TextField
+        type="password"
+        variant="outlined"
+        label="Password"
+        fullWidth
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Typography
+        variant="p"
+        color="primary.light"
+        onClick={() => props.setHaveAccount(true)}
+        sx={{ cursor: "pointer", "&:hover": { color: "blue" } }}
+      >
+        *Don't have an account? Register Here
+      </Typography>
+      <Button variant="outlined" onClick={submitHandler}>
+        Sign Up
+      </Button>
+    </>
+  );
+};
+
+export default Signup;
