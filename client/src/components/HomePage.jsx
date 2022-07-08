36441fa1340a70fedd38f3ccebfaf2eda1ace083
Sina Rafieefar <anisr95@yahoo.com>
2022-07-08 01:18:36 -0700

    Changed the look of the HomePage by adding background image and changing texts

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Button, Stack, Typography } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import bgImage from "../assets/expenseTracker.jpeg";
+import mongodbIcon from "../assets/mongodbIcon.png";
+import expressIcon from "../assets/expressIcon.png";
+import reactIcon from "../assets/reactIcon.png";
+import nodeIcon from "../assets/nodeIcon.png";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div>
-      <Box
+      {/* <Box
         sx={{
           display: "flex",
           flexDirection: "column",
@@ -34,6 +40,50 @@ const HomePage = () => {
           Authentication, Ability to create profile, Becoming more responsive
           for Mobile and Tablet use, and many more
         </Typography>
+      </Box> */}
+      <Box
+        className="bg"
+        sx={{
+          backgroundImage: `url("${bgImage}")`,
+        }}
+      ></Box>
+      <Box className="content">
+        <Container sx={{ textAlign: "left" }}>
+          <Typography gutterBottom variant="h2">
+            Expense Tracker
+          </Typography>
+          <Typography variant="h5">Stack Used:</Typography>
+          <Stack marginTop="2rem" spacing={4}>
+            <Stack direction="row" alignItems="center" spacing={3} width="80px">
+              <img src={mongodbIcon} alt="MongoDB Icon" width="80px" />
+              <Typography variant="h5"> MongoDB</Typography>
+            </Stack>
+            <Stack direction="row" alignItems="center" spacing={3} width="80px">
+              <img src={expressIcon} alt="express Icon" width="80px" />
+              <Typography variant="h5"> Express</Typography>
+            </Stack>
+            <Stack direction="row" alignItems="center" spacing={3} width="80px">
+              <img src={reactIcon} alt="react Icon" width="80px" />
+              <Typography variant="h5"> React</Typography>
+            </Stack>
+            <Stack direction="row" alignItems="center" spacing={3} width="80px">
+              <img src={nodeIcon} alt="node Icon" width="80px" />
+              <Typography variant="h5"> Node</Typography>
+            </Stack>
+            <Typography color="primary.dark" variant="h6">
+              Start by Loggin in
+            </Typography>
+          </Stack>
+          <Button
+            component={Link}
+            to="/auth/register"
+            sx={{ marginTop: "1rem", padding: "0.6rem 4rem" }}
+            large
+            variant="contained"
+          >
+            Login
+          </Button>
+        </Container>
       </Box>
     </div>
   );
